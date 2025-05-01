c019963accb38f124ae8cf725db29b18798786e7
이재홍 <leejh0813@kakao.com>
2025-05-01 14:51:06 +0900

    feat: 컬러 추가

--- Sources/JCommonKit/Color/Color.swift
+++ Sources/JCommonKit/Color/Color.swift
@@ -8,5 +8,51 @@
 import SwiftUI
 
 public extension Color {
-    static let testColor = Color("TestColor", bundle: .module)
+    
+    // MARK: Gray
+    
+    /// 191919
+    static let gray10 = Color("Gray10", bundle: .module)
+    /// 333333
+    static let gray20 = Color("Gray20", bundle: .module)
+    /// 4D4D4D
+    static let gray30 = Color("Gray30", bundle: .module)
+    /// 666666
+    static let gray40 = Color("Gray40", bundle: .module)
+    /// 808080
+    static let gray50 = Color("Gray50", bundle: .module)
+    /// 999999
+    static let gray60 = Color("Gray60", bundle: .module)
+    /// B3B3B3
+    static let gray70 = Color("Gray70", bundle: .module)
+    /// CCCCCC
+    static let gray80 = Color("Gray80", bundle: .module)
+    /// E6E6E6
+    static let gray90 = Color("Gray90", bundle: .module)
+    /// F2F2F2
+    static let gray95 = Color("Gray95", bundle: .module)
+    /// F8F8F8
+    static let gray97 = Color("Gray97", bundle: .module)
+    /// FFFFFF
+    static let gray100 = Color("Gray100", bundle: .module)
+    
+    // MARK: Primary
+    
+    /// 4B79F9
+    static let primary40 = Color("Primary40", bundle: .module)
+    /// 648BF9
+    static let primary60 = Color("Primary60", bundle: .module)
+    /// EDF2FF
+    static let primary95 = Color("Primary95", bundle: .module)
+    /// F7F9FF
+    static let primary97 = Color("Primary97", bundle: .module)
+    
+    // MARK: Secondary
+    
+    /// BF5EEC
+    static let secondary40 = Color("Secondary40", bundle: .module)
+    /// F9F0FF
+    static let secondary95 = Color("Secondary95", bundle: .module)
+    /// FDFAFF
+    static let secondary97 = Color("Secondary97", bundle: .module)
 }
