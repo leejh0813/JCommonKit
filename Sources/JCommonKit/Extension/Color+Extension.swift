b77ae590f64c33b69cb5bc4dbae5b31c92ddb3b9
이재홍 <leejh0813@kakao.com>
2025-05-03 07:51:14 +0900

    feat: Color Extension 추가

---
+++ Sources/JCommonKit/Extension/Color+Extension.swift
@@ -0,0 +1,56 @@
+//
+//  Color+Extension.swift
+//  DailyInsights
+//
+//  Created by 이재홍 on 8/4/24.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    /// Hex 문자열로 변환
+    /// - Returns: String
+    func toHex() -> String {
+        let uiColor = UIColor(self)
+        
+        guard let components = uiColor.cgColor.components, components.count >= 3 else {
+            return "#000000"
+        }
+        
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        let hex = String(format: "#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        
+        return hex
+    }
+    
+    /// Hex 문자열에서 Color 생성
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        let a, r, g, b: UInt64
+        
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
