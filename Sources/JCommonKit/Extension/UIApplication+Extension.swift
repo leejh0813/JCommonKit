beda97366af3f771a3ac5d0b226d90de3313136b
이재홍 <leejh0813@kakao.com>
2025-05-03 07:38:27 +0900

    feat: UIApplication Extension 추가

---
+++ Sources/JCommonKit/Extension/UIApplication+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIApplication+Extension.swift
+//  DailyInsights
+//
+//  Created by 이재홍 on 2024/06/11.
+//
+
+import UIKit
+
+extension UIApplication {
+    
+    var keyWindow: UIWindow? {
+        connectedScenes
+            .compactMap {
+                $0 as? UIWindowScene
+            }
+            .flatMap {
+                $0.windows
+            }
+            .first {
+                $0.isKeyWindow
+            }
+    }
+}
